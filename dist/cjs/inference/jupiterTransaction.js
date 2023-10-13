46c7cc1ec92b1a1f8eb01bcc51b9dc794886b60a
Marco Ordoñez <marco@evolbit.net>
2023-10-14 00:53:14 +0200

    WIP

--- dist/cjs/inference/jupiterTransaction.js
+++ dist/cjs/inference/jupiterTransaction.js
@@ -4,17 +4,21 @@ const tslib_1 = require("tslib");
 const lodash_1 = tslib_1.__importDefault(require("lodash"));
 const jupiterTransaction = async (props) => {
     const { instructions, tokens, walletAddress, connection } = props;
-    const swap = instructions.filter(i => i.type == 'JUPITER_SWAP');
-    // console.log('Effort on jupiterTransaction', (swap.length == 1))
+    const swap = instructions.filter((i) => i.type == "JUPITER_SWAP");
+    console.log("Effort on jupiterTransaction", JSON.stringify(instructions));
     if (swap.length == 1) {
-        const firstTransfer = lodash_1.default.first(instructions.filter(i => i.type == 'SPL_TRANSFER')).data;
-        const lastTransfer = lodash_1.default.last(instructions.filter(i => i.type == 'SPL_TRANSFER')).data;
-        const tokenFromObject = tokens.find(t => t.address == firstTransfer.tokenMint);
-        const tokenToObject = tokens.find(t => t.address == lastTransfer.tokenMint);
-        const fromAmount = tokenFromObject ? firstTransfer.amount / Math.pow(10, tokenFromObject.decimals) : firstTransfer.amount;
-        const toAmount = tokenToObject ? lastTransfer.amount / Math.pow(10, tokenToObject.decimals) : lastTransfer.amount;
+        const firstTransfer = lodash_1.default.first(instructions.filter((i) => i.type == "SPL_TRANSFER")).data;
+        const lastTransfer = lodash_1.default.last(instructions.filter((i) => i.type == "SPL_TRANSFER")).data;
+        const tokenFromObject = tokens.find((t) => t.address == firstTransfer.tokenMint);
+        const tokenToObject = tokens.find((t) => t.address == lastTransfer.tokenMint);
+        const fromAmount = tokenFromObject
+            ? firstTransfer.amount / Math.pow(10, tokenFromObject.decimals)
+            : firstTransfer.amount;
+        const toAmount = tokenToObject
+            ? lastTransfer.amount / Math.pow(10, tokenToObject.decimals)
+            : lastTransfer.amount;
         return {
-            type: 'JUPITER_SWAP',
+            type: "JUPITER_SWAP",
             data: {
                 tokenFrom: {
                     ...tokenFromObject,
@@ -23,8 +27,8 @@ const jupiterTransaction = async (props) => {
                 tokenTo: {
                     ...tokenToObject,
                     amount: toAmount,
-                }
-            }
+                },
+            },
         };
     }
     else
--- dist/esm/inference/jupiterTransaction.js
+++ dist/esm/inference/jupiterTransaction.js
@@ -1,17 +1,21 @@
 import _ from "lodash";
 const jupiterTransaction = async (props) => {
     const { instructions, tokens, walletAddress, connection } = props;
-    const swap = instructions.filter(i => i.type == 'JUPITER_SWAP');
-    // console.log('Effort on jupiterTransaction', (swap.length == 1))
+    const swap = instructions.filter((i) => i.type == "JUPITER_SWAP");
+    console.log("Effort on jupiterTransaction", JSON.stringify(instructions));
     if (swap.length == 1) {
-        const firstTransfer = _.first(instructions.filter(i => i.type == 'SPL_TRANSFER')).data;
-        const lastTransfer = _.last(instructions.filter(i => i.type == 'SPL_TRANSFER')).data;
-        const tokenFromObject = tokens.find(t => t.address == firstTransfer.tokenMint);
-        const tokenToObject = tokens.find(t => t.address == lastTransfer.tokenMint);
-        const fromAmount = tokenFromObject ? firstTransfer.amount / Math.pow(10, tokenFromObject.decimals) : firstTransfer.amount;
-        const toAmount = tokenToObject ? lastTransfer.amount / Math.pow(10, tokenToObject.decimals) : lastTransfer.amount;
+        const firstTransfer = _.first(instructions.filter((i) => i.type == "SPL_TRANSFER")).data;
+        const lastTransfer = _.last(instructions.filter((i) => i.type == "SPL_TRANSFER")).data;
+        const tokenFromObject = tokens.find((t) => t.address == firstTransfer.tokenMint);
+        const tokenToObject = tokens.find((t) => t.address == lastTransfer.tokenMint);
+        const fromAmount = tokenFromObject
+            ? firstTransfer.amount / Math.pow(10, tokenFromObject.decimals)
+            : firstTransfer.amount;
+        const toAmount = tokenToObject
+            ? lastTransfer.amount / Math.pow(10, tokenToObject.decimals)
+            : lastTransfer.amount;
         return {
-            type: 'JUPITER_SWAP',
+            type: "JUPITER_SWAP",
             data: {
                 tokenFrom: {
                     ...tokenFromObject,
@@ -20,8 +24,8 @@ const jupiterTransaction = async (props) => {
                 tokenTo: {
                     ...tokenToObject,
                     amount: toAmount,
-                }
-            }
+                },
+            },
         };
     }
     else
